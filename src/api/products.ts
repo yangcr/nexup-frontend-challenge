f5c197d0cb0224af18ed73478c788c6d528729a5
Cristopher Yang <dg.cris.yang@gmail.com>
2026-03-10 19:10:10 -0300

    chore: mock data

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,6 +1,92 @@
 import { Product } from '../models/Product';
+import { ProductCategory } from '../models/ProductCategory';
+import { ProductStatus } from '../models/ProductStatus';
 
-// Change the return type if necessary
-export const getProductList = (): Product[] => {
-  return [];
-};
+const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'Apple',
+    category: ProductCategory.Fruit,
+    status: ProductStatus.Active,
+    price: 1.5,
+  },
+  {
+    id: 2,
+    name: 'Banana',
+    category: ProductCategory.Fruit,
+    status: ProductStatus.Active,
+    price: 0.75,
+  },
+  {
+    id: 3,
+    name: 'Mango',
+    category: ProductCategory.Fruit,
+    status: ProductStatus.Inactive,
+    price: 2.3,
+  },
+  {
+    id: 4,
+    name: 'Strawberry',
+    category: ProductCategory.Fruit,
+    status: ProductStatus.Active,
+    price: 3.99,
+  },
+  {
+    id: 5,
+    name: 'Broccoli',
+    category: ProductCategory.Vegetables,
+    status: ProductStatus.Active,
+    price: 1.2,
+  },
+  {
+    id: 6,
+    name: 'Carrot',
+    category: ProductCategory.Vegetables,
+    status: ProductStatus.Active,
+    price: 0.9,
+  },
+  {
+    id: 7,
+    name: 'Spinach',
+    category: ProductCategory.Vegetables,
+    status: ProductStatus.Inactive,
+    price: 1.75,
+  },
+  {
+    id: 8,
+    name: 'Tomato',
+    category: ProductCategory.Vegetables,
+    status: ProductStatus.Active,
+    price: 1.1,
+  },
+  {
+    id: 9,
+    name: 'Beef Steak',
+    category: ProductCategory.Meat,
+    status: ProductStatus.Active,
+    price: 12.5,
+  },
+  {
+    id: 10,
+    name: 'Chicken Breast',
+    category: ProductCategory.Meat,
+    status: ProductStatus.Active,
+    price: 7.8,
+  },
+  {
+    id: 11,
+    name: 'Pork Ribs',
+    category: ProductCategory.Meat,
+    status: ProductStatus.Inactive,
+    price: 9.99,
+  },
+  {
+    id: 12,
+    name: 'Lamb Chops',
+    category: ProductCategory.Meat,
+    status: ProductStatus.Active,
+    price: 15.0,
+  },
+];
+
+export const getProductList = (): Product[] => PRODUCTS;
